9432865f78b6928b1260a525731c90091e8f2f99
Nathan <nathan.froeh@gmail.com>
2019-08-14 14:55:18 -0600

    Add advantage against endpoints

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 Build Your Own Backend
 
 ## Endpoints
-localhost3000/:id 
+localhost3000/type
 will return pokemon by type
 
-localhost3000/:id?leader 
+localhost3000/type?strongest 
 will return strongest pokemon for specified type
 
+localhost3000/type?weakest 
+will return weakest pokemon for specified type
+
+localhost3000/advantage/type
+get all pokemon that are weak against specified type
+
+localhost3000/advantage/type?strongest
+get strongest pokemon that is weak against specified type
+
+localhost3000/advantage/type?weakest
+get weakest pokemon that is weak against specified type
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 
 
-// localhost3000/:id
+// localhost3000/type
 // id will be type id
 // get all pokemon by type
 
 
-// localhost3000/:id?leader
+// localhost3000/type?strongest
 // get strongest pokemon by type
+
+
+// localhost3000/type?weakest 
+// will return weakest pokemon for specified type
+
+
+// localhost/advantage/type
+// get all pokemon that are weak against specified type
+
+
+// localhost/advantage/type?strongest
+// get strongest pokemon that is weak against specified type
+
+// localhost/advantage/type?weakest
+// get weakest pokemon that is weak against specified type
+
 
