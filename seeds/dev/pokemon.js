f8f763c1c844bcebc167c06321118123366a1974
Nathan <nathan.froeh@gmail.com>
2019-08-15 14:43:24 -0600

    test

--- seeds/dev/pokemon.js
+++ seeds/dev/pokemon.js
@@ -1,54 +1,59 @@
-const data = require('./data');
+const data = require('../../data');
 
 
 exports.seed = knex => {
 
   const types = [
-    {name: 'normal', good_against: 'ghost'},
-    {name: 'fighting', good_against: 'normal'},
-    {name: 'ground', good_against: 'electric'},
-    {name: 'rock', good_against: 'ice'},
-    {name: 'bug', good_against: 'psychic'},
-    {name: 'ghost', good_against: 'bug'},
-    {name: 'fire', good_against: 'grass'},
-    {name: 'water', good_against: 'fire'},
-    {name: 'grass', good_against: 'rock'},
-    {name: 'electric', good_against: 'water'},
-    {name: 'psychic', good_against: 'poison'},
-    {name: 'ice', good_against: 'ground'},
-    {name: 'dragon', good_against: 'dragon'},
-    {name: 'fairy', good_against: 'fighting'}
+    {name: 'Normal', good_against: 'Ghost'},
+    {name: 'Fighting', good_against: 'Normal'},
+    {name: 'Ground', good_against: 'Electric'},
+    {name: 'Rock', good_against: 'Ice'},
+    {name: 'Bug', good_against: 'Psychic'},
+    {name: 'Ghost', good_against: 'Bug'},
+    {name: 'Fire', good_against: 'Grass'},
+    {name: 'Water', good_against: 'Fire'},
+    {name: 'Grass', good_against: 'Rock'},
+    {name: 'Electric', good_against: 'Water'},
+    {name: 'Psychic', good_against: 'Poison'},
+    {name: 'Ice', good_against: 'Ground'},
+    {name: 'Dragon', good_against: 'Dragon'},
+    {name: 'Fairy', good_against: 'Fighting'}
   ]
 
   return knex('pokemon').del()
     .then(() => knex('type').del())
     .then(() => {
       return Promise.all([
-        types.forEach(type => {
-          knex('type').insert({type},'id')
-            .then(typeId => {
-              return knex('pokemon').insert(() => {
-                const creatures = data.filter(pokemon => {
-                  if(pokemon.type === type) {
-                    return pokemon
-                  }
-                })
-                return creatures.map(pokemon => (
-                  {
-                    type_id: typeId,
-                    name: pokemon.name,
-                    hp: pokemon.HP,
-                    attack: pokemon.Attack,
-                    defense: pokemon.Defense,
-                    speed: pokemon.Speed
-                  }
-                ))
-              })
-            })
+        types.forEach(pokeType => {
+          console.log(pokeType)
+          knex('type').insert({ name: pokeType.name, good_against: pokeType.good_against },'id')
 
+            // .then(typeId => {
+            //   // console.log(typeId)
+            //   return knex('pokemon').insert(() => {
+            //     const creatures = data.filter(pokemon => {
+            //       // console.log(pokeType)
+            //       if(pokemon.type === pokeType.name) {
+            //         // console.log(pokeType, pokemon)
+            //         return pokemon
+            //       }
+            //     })
+            //     return creatures.map(pokemon => {
+            //       return {
+            //         type_id: typeId[0],
+            //         name: pokemon.name,
+            //         hp: pokemon.HP,
+            //         attack: pokemon.Attack,
+            //         defense: pokemon.Defense,
+            //         speed: pokemon.Speed
+            //       }
+            //     })
+            //   })
+            // })
+
+            .then(() => console.log('Database was seeded!'))
+            .catch(error => console.log(`Seed error ${error}`))
         })
-        .then(() => console.log('Database was seeded!'))
-        .catch(error => console.log(`Seed error ${error}`))
 
       ])
     })
