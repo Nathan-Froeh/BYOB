25a2127a909e2c65bf6738d72330b11fd1454946
Nathan <nathan.froeh@gmail.com>
2019-08-15 10:20:41 -0600

    Add all

--- migrations/20190815095810_initial.js
+++ migrations/20190815095810_initial.js
@@ -2,13 +2,25 @@
 exports.up = function(knex) {
   
   return Promise.all([
-    knex.schema.createTable('Type', (table) => {
+    knex.schema.createTable('type', table => {
       table.increments('id').primary()
       table.string('id')
       table.string('name')
       table.string('good against')
-      
+
       table.timestamps(true, true)
+    }),
+    knex.schema.createTable('pokemon', table => {
+      table.increments('id').primary()
+      table.string('id')
+      table.string('name')
+      table.string('hp')
+      table.string('attack')
+      table.string('defense')
+      table.string('speed')
+      table.integer('type_id').unsigned()
+      table.foreign('type_id')
+        .references('type.id')
     })
   ])
 
